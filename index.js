3543c586a7d5d8e615371d73af28b312514f755c
Cecilia Goss <ceci.goss@gmail.com>
2017-11-25 21:23:21 -0600

    require username and password for post request to server

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const qrCode = require('qrcode-npm');
 
-const app = express()
+const app = express();
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', 3000 || process.env.PORT );
 
 app.get('/', (req, res) => {
   var qr = qrCode.qrcode(4, 'M');
@@ -14,8 +14,27 @@ app.get('/', (req, res) => {
   let img = qr.createImgTag(4)
   let reg = /"([^"]*)"/g;
   let result = img.match(reg)[0];
-
   res.end(result);    // creates an <img> tag as text
+});
+
+app.post('/login', (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if (username && password) {
+    var qr = qrCode.qrcode(4, 'M');
+    qr.addData("hello");
+    qr.make();
+    res.writeHeader(200, {"Content-Type": "text/html"});  
+    console.log('broken');
+    let img = qr.createImgTag(4)
+    let reg = /"([^"]*)"/g;
+    let result = img.match(reg)[0];
+    res.end(result);
+  } else {
+    res.end('');
+  }
+  
 });
 
 
